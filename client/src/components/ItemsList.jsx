29569315337fd3daeb7a3198718cb87d74eb2970
Mirumo <mysteryperson520@gmail.com>
2021-12-29 14:27:38 +0800

    feat: search and borrow popup

--- client/src/components/ItemsList.jsx
+++ client/src/components/ItemsList.jsx
@@ -8,7 +8,31 @@ function ItemsList() {
   const { uid } = useParams();
   useEffect(() => {
     getItemsList(uid).then((response) => setList(response.data));
-  });
+  }, []);
+
+  const status = (no) => {
+    switch (no) {
+      case 0:
+        return '正常';
+      case 1:
+        return '已借出';
+      case 2:
+        return '損毀';
+      case 3:
+        return '缺件';
+      default:
+        return '未定義';
+    }
+  };
+
+  const borrow = (x) => {
+    const name = prompt('Please enter name: ');
+    const id = prompt('Please enter student id: ');
+
+    if (name && id) {
+      console.log(x, name, id);
+    }
+  };
 
   return (
     <>
@@ -16,18 +40,20 @@ function ItemsList() {
         list.code === 200 ? (
           <table cellPadding={10}>
             <thead>
-              <td>no</td>
-              <td>status</td>
-              <td>description</td>
-              <td>enable</td>
+              <tr>
+                <td>no</td>
+                <td>status</td>
+                <td>description</td>
+                <td>enable</td>
+              </tr>
             </thead>
             <tbody>
               {list.result.map((x) => (
-                <tr>
+                <tr key={x.no} onClick={() => borrow({ x })} style={{ cursor: 'pointer' }}>
                   <td>{x.no}</td>
-                  <td>{x.status}</td>
+                  <td>{status(x.status)}</td>
                   <td>{x.description}</td>
-                  <td>{x.enable}</td>
+                  <td>{x.enable ? '可借用' : '無法借用'}</td>
                 </tr>
               ))}
             </tbody>
@@ -36,7 +62,7 @@ function ItemsList() {
           <p>{list.msg}</p>
         )
       ) : (
-        <div>loading</div>
+        <div>loading...</div>
       )}
     </>
   );
--- client/src/util/getItemsList.js
+++ client/src/util/getItemsList.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 const getSetList = async (uid) => {
+  if (!uid) return null;
   try {
     const result = await axios.get('http://localhost:5000/api/v1/items/' + uid);
     return result;
   } catch (error) {
     console.log(error);
+    return null;
   }
 };
 
